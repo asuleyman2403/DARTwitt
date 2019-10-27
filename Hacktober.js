cdad342ef43a52fea5abc543a17f725852085fc9
Assyl Suleiman <35847051+asuleyman2403@users.noreply.github.com>
2019-10-28 01:11:59 +0600

    Create Hacktober.js

---
+++ Hacktober.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Hacktober.scss';
+
+class Hacktober extends Component {
+  render() {
+    return (<div>Hello world!</div>);
+  }
+}
+
+export default Hacktober;
